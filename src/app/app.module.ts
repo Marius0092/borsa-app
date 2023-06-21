3c7710d1b709b31a58b7173bca83f0fe16042c21
Mario Montante <mariom0092@gmail.com>
2023-06-21 15:14:28 +0200

    added stock-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { StocklistComponent } from './stocklist/stocklist.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { StockInputComponent } from './stock-input/stock-input.component';
 import { MaterialModule } from './material/material.module';
+import { StockDetailsComponent } from './stock-details/stock-details.component';
 
 @NgModule({
-  declarations: [AppComponent, StocklistComponent, StockInputComponent],
+  declarations: [AppComponent, StocklistComponent, StockInputComponent, StockDetailsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/stock-input/stock-input.component.ts
+++ src/app/stock-input/stock-input.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-stock-input',
   templateUrl: './stock-input.component.html',
-  styleUrls: ['./stock-input.component.css']
+  styleUrls: ['./stock-input.component.css'],
 })
 export class StockInputComponent {
-
+  value: any;
 }
