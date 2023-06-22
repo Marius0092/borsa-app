5664d977b54514e013d1949b778b32ca07c5390a
Mario Montante <mariom0092@gmail.com>
2023-06-22 16:16:21 +0200

    Added stock-details.component

--- src/app/service/stock.service.ts
+++ src/app/service/stock.service.ts
@@ -24,100 +24,108 @@ export class StockService {
 
     const profileUrl = `https://finnhub.io/api/v1/stock/profile2?symbol=${symbol}&token=${token}`;
     const quoteUrl = `https://finnhub.io/api/v1/quote?symbol=${symbol}&token=${token}`;
-    const insiderSentimentUrl = `https://finnhub.io/api/v1/stock/insider-sentiment?symbol=${symbol}&from=${lastDate}&to=${currentDate}&token=${token}`
+    const insiderSentimentUrl = `https://finnhub.io/api/v1/stock/insider-sentiment?symbol=${symbol}&from=${lastDate}&to=${currentDate}&token=${token}`;
 
-    this.http.get<any>(profileUrl).pipe(
-      switchMap((profileResponse) => {
-        return this.http.get<any>(quoteUrl).pipe(
-          switchMap((quoteResponse) => {
-            return this.http.get<any>(insiderSentimentUrl).pipe(
-              switchMap((insiderResponse) => {
-                const stockData: Stock = {
-                  nameCompany: profileResponse.name,
-                  displaySymbol: profileResponse.ticker,
-                  initialPrice: quoteResponse.pc,
-                  currentPrice: quoteResponse.c,
-                  logo: profileResponse.logo,
-                  currency: profileResponse.currency,
-                  currentDate: {
-                    change:
-                      insiderResponse?.data[insiderResponse.data.length - 1]
-                        .change,
-                    month:
-                      insiderResponse?.data[insiderResponse.data.length - 1]
-                        .month,
-                    year: insiderResponse?.data[insiderResponse.data.length - 1]
-                      .year,
-                  },
+    this.http
+      .get<any>(profileUrl)
+      .pipe(
+        switchMap((profileResponse) => {
+          return this.http.get<any>(quoteUrl).pipe(
+            switchMap((quoteResponse) => {
+              return this.http.get<any>(insiderSentimentUrl).pipe(
+                switchMap((insiderResponse) => {
+                  const stockData: Stock = {
+                    nameCompany: profileResponse.name,
+                    displaySymbol: profileResponse.ticker,
+                    initialPrice: quoteResponse.pc,
+                    currentPrice: quoteResponse.c,
+                    logo: profileResponse.logo,
+                    currency: profileResponse.currency,
+                    currentDate: {
+                      change:
+                        insiderResponse?.data[insiderResponse.data.length - 1]
+                          .change,
+                      month:
+                        insiderResponse?.data[insiderResponse.data.length - 1]
+                          .month,
+                      year: insiderResponse?.data[
+                        insiderResponse.data.length - 1
+                      ].year,
+                    },
 
-                  oneMonthAgo: {
-                    change:
-                      insiderResponse.data[insiderResponse.data.length - 2]
-                        ?.change,
-                    month:
-                      insiderResponse.data[insiderResponse.data.length - 2]
-                        ?.month,
-                    year: insiderResponse.data[insiderResponse.data.length - 2]
-                      ?.year,
-                  },
-                  twoMonthAgo: {
-                    change:
-                      insiderResponse.data[insiderResponse.data.length - 3]
-                        ?.change,
-                    month:
-                      insiderResponse.data[insiderResponse.data.length - 3]
-                        ?.month,
-                    year: insiderResponse.data[insiderResponse.data.length - 3]
-                      ?.year,
-                  }
-                };
+                    oneMonthAgo: {
+                      change:
+                        insiderResponse.data[insiderResponse.data.length - 2]
+                          ?.change,
+                      month:
+                        insiderResponse.data[insiderResponse.data.length - 2]
+                          ?.month,
+                      year: insiderResponse.data[
+                        insiderResponse.data.length - 2
+                      ]?.year,
+                    },
+                    twoMonthAgo: {
+                      change:
+                        insiderResponse.data[insiderResponse.data.length - 3]
+                          ?.change,
+                      month:
+                        insiderResponse.data[insiderResponse.data.length - 3]
+                          ?.month,
+                      year: insiderResponse.data[
+                        insiderResponse.data.length - 3
+                      ]?.year,
+                    },
+                  };
 
-                console.log(stockData);
-                console.log(insiderResponse);
+                  console.log(stockData);
+                  console.log(insiderResponse);
 
-                //Copia la lista attuale
-                const currentStocks = this.stocksSubject.value;
-                console.log(currentStocks);
+                  //Copia la lista attuale
+                  const currentStocks = this.stocksSubject.value;
+                  console.log(currentStocks);
 
-                //Pusha i dati dello stockData che otteniamo dalla risposta
-                currentStocks.push(stockData);
-                this.stocksSubject.next(currentStocks);
+                  //Pusha i dati dello stockData che otteniamo dalla risposta
+                  currentStocks.push(stockData);
+                  this.stocksSubject.next(currentStocks);
 
-                // Restituisci un observable vuoto (o un altro observable se necessario)
-                return of(null);
-              }),
-              catchError((error) => {
-                console.log('Error fetching insider sentiment:', error);
-                // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
-                return of(null);
-              })
-            );
-          }),
-          catchError((error) => {
-            console.log('Error fetching stock quote:', error);
-            // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
-            return of(null);
-          })
-        );
-      }),
-      catchError((error) => {
-        console.log('Error fetching stock profile:', error);
-        // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
-        return of(null);
-      })
-    ).subscribe();
+                  // Restituisci un observable vuoto (o un altro observable se necessario)
+                  return of(null);
+                }),
+                catchError((error) => {
+                  console.log('Error fetching insider sentiment:', error);
+                  // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
+                  return of(null);
+                })
+              );
+            }),
+            catchError((error) => {
+              console.log('Error fetching stock quote:', error);
+              // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
+              return of(null);
+            })
+          );
+        }),
+        catchError((error) => {
+          console.log('Error fetching stock profile:', error);
+          // Gestisci l'errore qui, ad esempio restituendo un observable con un messaggio di errore
+          return of(null);
+        })
+      )
+      .subscribe();
   }
 
   deleteStock(stock: Stock) {
     const currentStocks = this.stocksSubject.getValue();
-    const updatedStocks = currentStocks.filter((s) => s.displaySymbol !== stock.displaySymbol);
+    const updatedStocks = currentStocks.filter(
+      (s) => s.displaySymbol !== stock.displaySymbol
+    );
     this.stocksSubject.next(updatedStocks);
   }
 
-  formatDate (date: Date): string {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
   }
 }
--- src/app/stock-details/stock-details.component.ts
+++ src/app/stock-details/stock-details.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Stock } from '../model/stock.model';
+import { StockService } from '../service/stock.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-stock-details',
   templateUrl: './stock-details.component.html',
-  styleUrls: ['./stock-details.component.css']
+  styleUrls: ['./stock-details.component.css'],
 })
-export class StockDetailsComponent {
+export class StockDetailsComponent implements OnInit {
+  public stockData: Stock | null = null;
 
+  constructor(private stockService: StockService, private location: Location) {}
+
+  ngOnInit(): void {
+    this.stockService.stocks$.subscribe((stocks: Stock[]) => {
+      // Assuming you want to display the first stock in the list
+      if (stocks.length > 0) {
+        this.stockData = stocks[0];
+      }
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
