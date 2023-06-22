4839efdf6add8df36ffae8a0b522d9d6f71c94d3
Mario Montante <mariom0092@gmail.com>
2023-06-22 16:30:09 +0200

    added unsubscribe on stock-details.component

--- src/app/stock-details/stock-details.component.ts
+++ src/app/stock-details/stock-details.component.ts
@@ -1,28 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Stock } from '../model/stock.model';
 import { StockService } from '../service/stock.service';
 import { Location } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-stock-details',
   templateUrl: './stock-details.component.html',
   styleUrls: ['./stock-details.component.css'],
 })
-export class StockDetailsComponent implements OnInit {
+export class StockDetailsComponent implements OnInit, OnDestroy {
   public stockData: Stock | null = null;
+  private stocksSubscription: Subscription | undefined;
 
   constructor(private stockService: StockService, private location: Location) {}
 
   ngOnInit(): void {
-    this.stockService.stocks$.subscribe((stocks: Stock[]) => {
-      // Assuming you want to display the first stock in the list
-      if (stocks.length > 0) {
-        this.stockData = stocks[0];
+    this.stocksSubscription = this.stockService.stocks$.subscribe(
+      (stocks: Stock[]) => {
+        // Assuming you want to display the first stock in the list
+        if (stocks.length > 0) {
+          this.stockData = stocks[0];
+        }
       }
-    });
+    );
   }
 
   goBack(): void {
     this.location.back();
+  }
+
+  ngOnDestroy(): void {
+    if (this.stocksSubscription) {
+      this.stocksSubscription.unsubscribe();
+    }
   }
 }
